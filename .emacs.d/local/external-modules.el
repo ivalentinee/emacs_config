b7b039a0dc0532670c59c5ed61d85177ce60c2b6
Emperor <valentine.emperor@gmail.com>
2014-01-10 13:33:09 +0400

    included AUCTeX

--- .emacs.d/local/external-modules.el
+++ .emacs.d/local/external-modules.el
@@ -37,6 +37,10 @@
 (ac-config-default)
 (auto-complete-mode t)
 
+;; AucTeX
+(load "auctex.el" nil t t)
+(load "preview-latex.el" nil t t)
+
 ;; Parentheses mode
 (require 'highlight-parentheses)
 (define-globalized-minor-mode global-highlight-parentheses-mode
