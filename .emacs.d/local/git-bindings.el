76b30744fa086fe072acb4739819c3397ccc75a1
Emperor <valentine.emperor@gmail.com>
2014-02-12 00:39:24 +0400

    Some more git bindings

--- .emacs.d/local/git-bindings.el
+++ .emacs.d/local/git-bindings.el
@@ -3,4 +3,7 @@
 (global-set-key (kbd "C-c g b") 'mo-git-blame-current)
 (global-set-key (kbd "C-c g q") 'mo-git-blame-quit)
 
+(global-set-key (kbd "C-c g s") 'magit-status)
+(global-set-key (kbd "C-c g c") 'magit-commit)
+
 (provide 'git-bindings)
