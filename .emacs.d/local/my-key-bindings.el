ee7bb364b2c2db8950bdc6b2fc4ebea5810860a3
Emperor <valentine.emperor@gmail.com>
2014-02-12 00:40:35 +0400

    backward-kill-word & paragraph move bindings

--- .emacs.d/local/my-key-bindings.el
+++ .emacs.d/local/my-key-bindings.el
@@ -12,4 +12,14 @@
 ;; iBuffer
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
+;; backward-kill-word
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-x\C-k" 'kill-region)
+(global-set-key "\C-c\C-k" 'kill-region)
+
+;; paragraphs forward & baskward
+(global-set-key "\M-p" 'backward-paragraph)
+(global-set-key "\M-n" 'forward-paragraph)
+
+
 (provide 'my-key-bindings)
