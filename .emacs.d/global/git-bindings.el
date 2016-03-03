7e09f000712078c2df243a55c61bf031a0964bba
Emperor <valentine.emperor@gmail.com>
2016-03-03 12:11:29 +0300

    changed magit-branch-manager to magit-checkout

--- .emacs.d/global/git-bindings.el
+++ .emacs.d/global/git-bindings.el
@@ -6,6 +6,6 @@
 (global-set-key (kbd "C-c g s") 'magit-status)
 (global-set-key (kbd "C-c g c") 'magit-commit)
 (global-set-key (kbd "C-c g l") 'magit-log)
-(global-set-key (kbd "C-c g m") 'magit-branch-manager)
+(global-set-key (kbd "C-c g m") 'magit-checkout)
 
 (provide 'git-bindings)
