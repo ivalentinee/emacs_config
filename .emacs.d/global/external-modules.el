d8425b5294a05d571bcd43c1ef89061603373363
Emperor <valentine.emperor@gmail.com>
2016-10-04 00:16:18 +0400

    removed git-commit-mode and git-rebase-mode

--- .emacs.d/global/external-modules.el
+++ .emacs.d/global/external-modules.el
@@ -112,6 +112,7 @@
 ;; multiple-cursors
 (require 'multiple-cursors)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-+") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
