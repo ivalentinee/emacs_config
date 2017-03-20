46dac675e8944ef0d2f76b920fab86a5268313a1
Emperor <valentine.emperor@gmail.com>
2017-03-20 11:27:59 +0400

    removed 'gnu' from package-archives list
    caused trouble with failing signature check

--- .emacs.d/global/package-settings.el
+++ .emacs.d/global/package-settings.el
@@ -1,8 +1,7 @@
 ;; package-settings.el
 
 (require 'package)
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")))
 
 (package-initialize)
 
