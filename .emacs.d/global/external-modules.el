397b03cfaf18d22d20e8b27c01bba8e970752ca8
Emperor <valentine.emperor@gmail.com>
2017-09-27 15:23:04 +0400

    moved web-mode associations to associations settings file

--- .emacs.d/global/external-modules.el
+++ .emacs.d/global/external-modules.el
@@ -145,13 +145,6 @@
 
 ;; web-mode
 (require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-hook 'web-mode-hook
           (lambda ()
             ;; short circuit js mode and just do everything in jsx-mode
