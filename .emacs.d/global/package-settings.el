d9819b292c07bb8a685b0504aff6bd9b2b7cc922
Emperor <valentine.emperor@gmail.com>
2018-06-01 11:16:56 +0400

    added gnu to package archives

--- .emacs.d/global/package-settings.el
+++ .emacs.d/global/package-settings.el
@@ -3,7 +3,8 @@
 
 ;;; Code:
 (require 'package)
-(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")))
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 (package-initialize)
 
