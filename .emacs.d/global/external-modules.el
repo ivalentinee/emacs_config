5ef1e5ad9d5a8c8cba887d519921dbf512e26d3b
Emperor <valentine.emperor@gmail.com>
2018-08-14 14:54:14 +0400

    Switched to helm-buffer-list for C-x b

--- .emacs.d/global/external-modules.el
+++ .emacs.d/global/external-modules.el
@@ -99,6 +99,7 @@
 (helm-mode 1)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x b") 'helm-buffers-list)
 
 ;; multiple-cursors
 (require 'multiple-cursors)
