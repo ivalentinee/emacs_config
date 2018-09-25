17d1ad0b231e64a01fcf8e6293d705b3c010a056
Emperor <valentine.emperor@gmail.com>
2018-09-25 19:02:51 +0400

    API Blueprint files highlighted as markdown

--- .emacs.d/global/my-file-associations.el
+++ .emacs.d/global/my-file-associations.el
@@ -4,10 +4,11 @@
 
 ;; Setup markdown-mode for *.md
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.apib\\'" . markdown-mode))
 
 ;; Setup ruby-mode for all ruby files
 (add-to-list 'auto-mode-alist
-               '("\\.\\(?:gemspec\\|irbrc\\|gemrc\\|rake\\|rb\\|ru\\|thor\\)\\'" . ruby-mode))
+             '("\\.\\(?:gemspec\\|irbrc\\|gemrc\\|rake\\|rb\\|ru\\|thor\\)\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist
                '("\\(Capfile\\|Gemfile\\(?:\\.[a-zA-Z0-9._-]+\\)?\\|[rR]akefile\\)\\'" . ruby-mode))
 
