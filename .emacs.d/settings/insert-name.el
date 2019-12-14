744df1cbe7ed282c433032a4bc85c67b4f401176
Emperor <valentine.emperor@gmail.com>
2019-12-14 12:17:05 +0400

    Insert and copy project-relative filename

--- .emacs.d/settings/insert-name.el
+++ .emacs.d/settings/insert-name.el
@@ -1,10 +1,30 @@
 (defun insert-file-name ()
-  "Inserts current buffer filename"
+  "Insert current buffer filename."
   (interactive)
   (insert (buffer-file-name)))
 
+(defun insert-project-file-name ()
+  "Insert current buffer project filename."
+  (interactive)
+  (if (and
+       (fboundp 'projectile-project-p)
+       (string-prefix-p (projectile-project-p) (buffer-file-name)))
+      (insert (substring (buffer-file-name)
+                         (length (projectile-project-p))
+                         (length (buffer-file-name))))))
+
+(defun copy-project-file-name ()
+  "Copy current buffer project filename to killring."
+  (interactive)
+  (if (and
+       (fboundp 'projectile-project-p)
+       (string-prefix-p (projectile-project-p) (buffer-file-name)))
+      (kill-new (substring (buffer-file-name)
+                           (length (projectile-project-p))
+                           (length (buffer-file-name))))))
+
 (defun insert-buffer-name ()
-  "Inserts current buffer name"
+  "Insert current buffer name."
   (interactive)
   (insert (buffer-name)))
 
